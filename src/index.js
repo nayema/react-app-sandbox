6d54202a938447ca59d4364e66e5bdfa90c6d250
Nayema Nasir <nayema.nasir89@gmail.com>
2019-05-25 00:51:02 -0400

    Refactor app

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { Provider } from "react-redux";
+import { Provider } from 'react-redux'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import { ThemeProvider } from '@material-ui/styles'
-import App from './App'
+import { App } from './App'
 import theme from './theme'
 
 import store from './store'
